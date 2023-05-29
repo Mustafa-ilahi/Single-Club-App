3e6c955e2f880542e1cc23d3e508f8a09ae5ea1c
Mustafa ilahi <mustafailahi586@gmail.com>
2023-05-28 18:30:04 -0800

    splash screen done

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Login from './src/screens/Login';
 import {colors, sizes} from './src/services';
 import MainNavigator from './src/services/config/navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View style={styles.container}>
-      {/* <Login /> */}
       <MainNavigator />
     </View>
   );
